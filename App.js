af81fbcc33af3b09850a7ddabb23232202be0831
Park-Ji-Eun <penny0123@naver.com>
2020-12-25 03:44:11 +0900

    Change style

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default class extends React.Component {
   getWeather = async (latitude, longitude) => {
     const {
       data: {
-        main: {temp},
+        main: { temp },
         weather
       }
     } = await axios.get(
@@ -29,7 +29,7 @@ export default class extends React.Component {
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
-      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
+      const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
     } catch (error) {
       Alert.alert("Can't find your location.", "Please check your location permissions.");
@@ -39,7 +39,7 @@ export default class extends React.Component {
     this.getLocation();
   }
   render() {
-    const {isLoading, temp, condition} = this.state;
+    const { isLoading, temp, condition } = this.state;
     return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition} />;
   }
 }
--- Loading.js
+++ Loading.js
@@ -2,24 +2,24 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function Loading() {
-    return (
-        <View style={styles.container}>
-          <Text style={styles.text}>Getting the weather</Text>
-        </View>
-      );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Getting the weather</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "flex-end",
-        paddingHorizontal: 30,
-        paddingVertical: 100,
-        backgroundColor: "#fab1a0"
-      },
-      text: {
-        color: "#ffffff",
-        fontSize: 35,
-        fontWeight: "bold"
-      }
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    backgroundColor: "#fab1a0"
+  },
+  text: {
+    color: "#ffffff",
+    fontSize: 35,
+    fontWeight: "bold"
+  }
 })
